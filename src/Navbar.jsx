d6c4d145cea38f1aac42574d1b51ddd7c4f8de87
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-09-14 00:15:48 +0530

    onScroll

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
 class NavBar extends React.Component {
+	constructor() {
+		super();
+		this.state = { scrollY: 0 };
+	}
+
+	onScroll = () => {
+		const scrollY = window.scrollY;
+		this.setState({ scrollY });
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.onScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.onScroll);
+	}
+
 	render() {
+		const { scrollY } = this.state;
+		let { background, color } = { background: 'transparent', color: 'white' };
+		if (window.innerWidth < 700 || scrollY > 0) {
+			background = 'white';
+			color = 'black';
+		} else {
+			background = 'transparent';
+			color = 'white';
+		}
+
 		return (
-			<Navbar variant="dark" expand="lg" fixed="top" style={{ background: 'transparent' }}>
+			<Navbar variant="light" expand="lg" fixed="top" style={{ background }}>
+				<Navbar.Brand href="#header" style={{ fontSize: '16px', color }}>CLASHPERK</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 
 				{/* Collapse Navbar */}
@@ -12,14 +41,14 @@ class NavBar extends React.Component {
 
 					{/* Left Side Menu */}
 					<Nav className="mr-auto" >
-						<Nav.Link style={{ color: 'white' }} href="/">HOME</Nav.Link>
-						<Nav.Link style={{ color: 'white' }} href="#features">FEATURES</Nav.Link>
+						<Nav.Link style={{ color }} href="/">HOME</Nav.Link>
+						<Nav.Link style={{ color }} href="#features">FEATURES</Nav.Link>
 					</Nav>
 
 					{/* Right Side Menu */}
 					<Nav>
-						<Nav.Link style={{ color: 'white' }} href="#teams">TEAMS</Nav.Link>
-						<Nav.Link style={{ color: 'white' }} href="#login">LOGIN</Nav.Link>
+						<Nav.Link style={{ color }} href="#teams">TEAMS</Nav.Link>
+						<Nav.Link style={{ color }} href="#login">LOGIN</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar>
