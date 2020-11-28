df45cbff29b116b664050dbba19cb7f0ae204125
SUVAJIT <suvajit.me@gmail.com>
2020-11-28 23:21:07 +0530

    use browser router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
